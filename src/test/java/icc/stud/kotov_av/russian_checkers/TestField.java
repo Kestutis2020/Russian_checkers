3633773dd9724537babc3928f2545e993ccf9bb5
Mad-User <kotov.andrey01@mail.ru>
2020-04-11 16:41:22 +0300

    add TestField

---
+++ src/test/java/icc/stud/kotov_av/russian_checkers/TestField.java
@@ -0,0 +1,16 @@
+package icc.stud.kotov_av.russian_checkers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestField {
+
+    @Test
+    public void testWrite() {
+        Field tFd = new Field();
+
+        assertEquals(-2, tFd.writeCell(0, 0, Cell.EMPTY));
+        assertEquals(0, tFd.writeCell(0, 1, Cell.BLACK_CHECKER));
+    }
+}

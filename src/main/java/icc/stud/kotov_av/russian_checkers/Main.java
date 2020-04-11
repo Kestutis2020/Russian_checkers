900745820a1f033764719fae0ad65b9b1b1bfa05
Mad-User <kotov.andrey01@mail.ru>
2020-04-11 16:24:02 +0300

    add TODO structure

--- src/main/java/icc/stud/kotov_av/russian_checkers/Main.java
+++ src/main/java/icc/stud/kotov_av/russian_checkers/Main.java
@@ -2,4 +2,20 @@ package icc.stud.kotov_av.russian_checkers;
 
 public class Main {
 
+    // TODO(inicialization field)
+    // TODO(inicialization window)
+
+    public static void main(String[] args) {
+        try {
+            while (true) {
+                // TODO(get input) 
+                // TODO(logic)
+                // TODO(print)
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        } finally {
+            // TODO(close all what opened)
+        }
+    }
 }
